1ff058a3fed9f786d45dba70ecb31c98b540b1c8
Draghici Florin <d_raghici@yahoo.com>
2023-11-21 21:37:38 +0200

    Add functionality for: Adding client, adding service product, Adding and removal for productTypes

--- iCustomerCareSystem/ViewModels/AddOrEditClientViewModel.cs
+++ iCustomerCareSystem/ViewModels/AddOrEditClientViewModel.cs
@@ -1,12 +1,18 @@
-﻿using iCustomerCareSystem.Models;
+﻿using iCustomerCareSystem.Data;
+using iCustomerCareSystem.Models;
+using Prism.Commands;
+using System;
 using System.ComponentModel;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace iCustomerCareSystem.ViewModels
 {
     public class AddOrEditClientViewModel: BasePopupViewModel, INotifyPropertyChanged
     {
         private Client _client;
+        private readonly ClientsDbContext _clientsDbContext;
 
         public Client Client
         {
@@ -18,11 +24,50 @@ namespace iCustomerCareSystem.ViewModels
             }
         }
 
-        public AddOrEditClientViewModel()
+        public ICommand SaveCommand { get; }
+        public event EventHandler ClientAddedSuccessfully;
+
+        public AddOrEditClientViewModel(ClientsDbContext clientsDbContext, Client? client = null)
         {
-                
+            _clientsDbContext = clientsDbContext;
+            _client = client ?? new Client();
+            SaveCommand = new DelegateCommand(async () => await SaveClientAsync());
         }
 
+        private async Task SaveClientAsync()
+        {
+            if (Client != null)
+            {
+                if (_client.ClientId == 0)
+                {
+                    _clientsDbContext.Clients.Add(Client);
+                }
+                else
+                {
+                    _clientsDbContext.Clients.Update(Client);
+                }
+
+                await SaveClientAsync(_clientsDbContext);
+            }
+        }
+        private async Task SaveClientAsync(ClientsDbContext clientsDbContext)
+        {
+            bool isSuccess = false;
+            try
+            {
+                await clientsDbContext.SaveChangesAsync();
+                isSuccess = true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            if (isSuccess)
+            {
+                ClientAddedSuccessfully?.Invoke(this, EventArgs.Empty);
+                ChildWindow?.Close();
+            }
+        }
 
         public event PropertyChangedEventHandler? PropertyChanged;
         protected void OnPropertyChanged(string propertyName)
